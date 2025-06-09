6d665cf801d280e64cb287173db5c0fe07ab81bf
Maksymilian Kafka <m.kafka00@gmail.com>
2025-06-09 23:54:54 +0200

    Gameplay Tags setup 🏷️
    Added Tag container and Asset Manager class set in DefaultEngine.ini

---
+++ Source/SaovYmladda/Private/Core/FSYGameplayTags.cpp
@@ -0,0 +1,28 @@
+﻿#include "Core/FSYGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+FSYGameplayTags FSYGameplayTags::GameplayTags;
+
+/*
+Template to copy:
+GameplayTags. = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName( "" ),
+		FString( "" ) );
+*/
+
+void FSYGameplayTags::InitializeNativeGameplayTags()
+{
+	/**	---Ability Tags--- **/
+
+	GameplayTags.Abilities_Action_Jump = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName( "Abilities.Action.Jump" ),
+		FString( "Jump Ability Tag" ) );
+
+
+	/** ---State Tags--- **/
+
+	GameplayTags.State_Character_InAction_Jumping = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName( "State.Character.InAction.Jumping" ),
+		FString( "Tag added when the character is jumping" ) );
+}
